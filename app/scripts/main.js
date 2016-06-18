0a65ebe6ff3454071902e4d9fa776a5727cf542d
Yi Chen <easoncy435@gmail.com>
2016-06-17 21:13:27 -0400

    modal update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCxRkGJI20d-SRzTlFYnmU94Jnfj7oCJbw",
-  authDomain: "scorching-fire-2347.firebaseapp.com",
-  databaseURL: "https://scorching-fire-2347.firebaseio.com",
-  storageBucket: "scorching-fire-2347.appspot.com",};
+  apiKey: 'AIzaSyCxRkGJI20d-SRzTlFYnmU94Jnfj7oCJbw',
+  authDomain: 'scorching-fire-2347.firebaseapp.com',
+  databaseURL: 'https://scorching-fire-2347.firebaseio.com',
+  storageBucket: 'scorching-fire-2347.appspot.com',};
 
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
 
-$( document ).ready(function() {
+  $( document ).ready(function() {
 
     $.fn.serializeObject = function() {
       var o = {};
@@ -33,23 +33,37 @@ $( document ).ready(function() {
 
 //Auth State header
 firebase.auth().onAuthStateChanged(function(user) {
-  var headerLoginBtn = $("#headerLoginBtn");
-  var headerSignupBtn = $("#headerSignupBtn");
-  var headerUserBtn = $("#headerUserBtn");
-  var headerUsername = $("#headerUsername");
-  var headerLogoutBtn = $("#headerLogoutBtn");
+  var headerLoginBtn = $('#headerLoginBtn');
+  var headerSignupBtn = $('#headerSignupBtn');
+  var headerUserBtn = $('#headerUserBtn');
+  var headerUsername = $('#headerUsername');
+  var headerLogoutBtn = $('#headerLogoutBtn');
+  var modalCreateEventBtn = $('#modalCreateEventBtn');
 
   if (user) {
+    console.log(user);
+
     firebase.database().ref('/users/' + user.uid).once('value').then(function(snapshot) {
-      headerLoginBtn.addClass("hide");
-      headerSignupBtn.addClass("hide");
+      headerLoginBtn.addClass('hide');
+      headerSignupBtn.addClass('hide');
       headerUsername.text('Hi, ' + snapshot.val().username).append('<span class="caret"></span>');
-      headerUserBtn.removeClass("hide");
+      headerUserBtn.removeClass('hide');
+    });
+    modalCreateEventBtn.off('click');
+    modalCreateEventBtn.click(function(){
+      modalCreateEvent.modal('show');
     });
   } else {
-    headerLoginBtn.removeClass("hide");
-    headerSignupBtn.removeClass("hide");
-    headerUserBtn.addClass("hide");
+    console.log('no user');
+
+    headerLoginBtn.removeClass('hide');
+    headerSignupBtn.removeClass('hide');
+    headerUserBtn.addClass('hide');
+
+    modalCreateEventBtn.off('click');
+    modalCreateEventBtn.click(function(){
+      modalSignup.modal('show');
+    });
   }
 
   headerLogoutBtn.click(function(){
@@ -66,9 +80,8 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 //event table
 firebase.database().ref('events/').once('value').then(function(snapshot){
-  console.log(snapshot.val());
   $.each(snapshot.val(), function( key, value ) {
-    $("#showEvent").append('<div class="col-xs-12 col-md-6"><div class="thumbnail"><div class="caption"><p>' + 
+    $('#showEvent').append('<div class="col-xs-12 col-md-6"><div class="thumbnail"><div class="caption"><p>' + 
       value.eventStart
       + '</p><h3>' +
       value.eventTitle
@@ -88,16 +101,16 @@ IssueTracker.prototype = {
     this.issues.push(issue);
   },
   retrieve: function () {
-    var message = "";
+    var message = '';
     switch (this.issues.length) {
       case 0:
         // do nothing because message is already ""
         break;
         case 1:
-        message = "Please correct the following issue:\n" + this.issues[0];
+        message = 'Please correct the following issue:\n' + this.issues[0];
         break;
         default:
-        message = "Please correct the following issues:\n" + this.issues.join("\n");
+        message = 'Please correct the following issues:\n' + this.issues.join('\n');
         console.log(this.issues);
         console.log(message);
         break;
@@ -107,47 +120,64 @@ IssueTracker.prototype = {
   };
 
 //modal signup
-var modalSignupBtn = $("#modalSignupBtn");
+var modalSignupBtn = $('#modalSignupBtn');
+var modalSignup = $('#modalSignup');
+var modalRLogin = $('#modalRLogin');
+
+modalRLogin.click(function(){
+    modalSignup.modal('hide');
+    modalLogin.modal('show');
+});
 
 modalSignupBtn.click(function(){
 
-  var modalSignupName = $("#modalSignupName");
-  var modalSignupEmail = $("#modalSignupEmail");
-  var modalSignupPassword = $("#modalSignupPassword");
+  var modalSignupName = $('#modalSignupName');
+  var modalSignupJob = $('#modalSignupJob');
+  var modalSignupEmail = $('#modalSignupEmail');
+  var modalSignupPassword = $('#modalSignupPassword');
 
   //get input value
   var modalSignupNameVal = modalSignupName.val();
+  var modalSignupJobVal = modalSignupJob.val();
   var modalSignupEmailVal = modalSignupEmail.val();
   var modalSignupPasswordVal = modalSignupPassword.val();
 
   //add issue tracker
   var modalSignupNameTraker = new IssueTracker();
+  var modalSignupJobTraker = new IssueTracker();
   var modalSignupEmailTraker = new IssueTracker();
   var modalSignupPasswordTraker = new IssueTracker();
 
   function checkrequirements(){
-    if(modalSignupNameVal.length< 3){
-      modalSignupNameTraker.add("Your name should be greater than 3 characters.");
-    } else if(modalSignupNameVal.length > 20){
-      modalSignupNameTraker.add("Your name should be fewer than 20 characters.");
+    if(modalSignupNameVal && modalSignupNameVal.length< 3){
+      modalSignupNameTraker.add('Your name should be greater than 3 characters.');
+    } else if(modalSignupNameVal && modalSignupNameVal.length > 20){
+      modalSignupNameTraker.add('Your name should be fewer than 20 characters.');
+    }
+
+    if(modalSignupJobVal && modalSignupJobVal.length <3){
+      modalSignupJobTraker.add('Your job title should be greater than 3 characters.');
+    } else if(modalSignupJobVal && modalSignupJobVal.length >20){
+      modalSignupJobTraker.add('Your job title should be fewer than 20 characters.');
     }
 
     if(!validateEmail(modalSignupEmailVal)) {
-      modalSignupEmailTraker.add("Email address is badly formatted.");
+      modalSignupEmailTraker.add('Email address is badly formatted.');
     }
 
     if(modalSignupPasswordVal.length<5){
-      modalSignupPasswordTraker.add("Password should be greater than 5 characters.");
+      modalSignupPasswordTraker.add('Password should be greater than 5 characters.');
     } else if(modalSignupPasswordVal.length>20){
-      modalSignupPasswordTraker.add("Password should be fewer than 20 characters.");
+      modalSignupPasswordTraker.add('Password should be fewer than 20 characters.');
     }
   };
 
-  function createUser(name, email, password){
+  function createUser(name, job, email, password){
     firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
-      writeUserData(user.uid, name);
+      writeUserData(user.uid, name, job);
       console.log('successfully created the user.');
       console.log(user);
+      modalSignup.modal('hide');
     }).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -155,22 +185,25 @@ modalSignupBtn.click(function(){
     });
   }
 
-  function writeUserData(userId, name) {
+  function writeUserData(userId, name, job) {
     firebase.database().ref('users/' + userId).set({
-      username: name
+      username: name,
+      job: job
     });
   }
 
   checkrequirements();
 
   var modalSignupNameIssue = modalSignupNameTraker.retrieve();
+  var modalSignupJobIssue = modalSignupJobTraker.retrieve();
   var modalSignupEmailIssue = modalSignupEmailTraker.retrieve();
   var modalSignupPasswordIssue = modalSignupPasswordTraker.retrieve();
 
   if(modalSignupNameIssue.length + modalSignupEmailIssue.length + modalSignupPasswordIssue.length === 0){
-    createUser(modalSignupNameVal, modalSignupEmailVal, modalSignupPasswordVal);
+    createUser(modalSignupNameVal, modalSignupJobVal, modalSignupEmailVal, modalSignupPasswordVal);
   } else {
     modalSignupName[0].setCustomValidity(modalSignupNameIssue);
+    modalSignupJob[0].setCustomValidity(modalSignupJobIssue);
     modalSignupEmail[0].setCustomValidity(modalSignupEmailIssue);
     modalSignupPassword[0].setCustomValidity(modalSignupPasswordIssue);
   };
@@ -179,16 +212,17 @@ modalSignupBtn.click(function(){
 
 
 //modal login
-var modalLoginBtn = $("#modalLoginBtn");
+var modalLoginBtn = $('#modalLoginBtn');
+var modalLogin = $('#modalLogin');
 
 modalLoginBtn.click(function(){
 
-  var modalLoginEmail = $("#modalLoginEmail");
-  var modalLoginPassword = $("#modalLoginPassword");
+  var modalLoginEmail = $('#modalLoginEmail');
+  var modalLoginPassword = $('#modalLoginPassword');
 
   //get input val value
-  var modalLoginEmailVal = $("#modalLoginEmail").val();
-  var modalLoginPasswordVal = $("#modalLoginPassword").val();
+  var modalLoginEmailVal = $('#modalLoginEmail').val();
+  var modalLoginPasswordVal = $('#modalLoginPassword').val();
 
   //add issue tracker
   var modalLoginEmailTraker = new IssueTracker();
@@ -196,19 +230,20 @@ modalLoginBtn.click(function(){
 
   function checkrequirements(){
     if(!validateEmail(modalLoginEmailVal)) {
-      modalLoginEmailTraker.add("Email address is badly formatted.");
+      modalLoginEmailTraker.add('Email address is badly formatted.');
     }
 
     if(modalLoginPasswordVal.length<5){
-      modalLoginPasswordTraker.add("Password should be greater than 5 characters.");
+      modalLoginPasswordTraker.add('Password should be greater than 5 characters.');
     } else if(modalLoginPasswordVal.length>20){
-      modalLoginPasswordTraker.add("Password should be fewer than 20 characters.");
+      modalLoginPasswordTraker.add('Password should be fewer than 20 characters.');
     }
   };
 
   function login(email, password){
     firebase.auth().signInWithEmailAndPassword(email, password).then(function(user){
-      console.log("successfully log in.");
+      console.log('successfully log in.');
+      modalLogin.modal('hide');
     }).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -231,24 +266,84 @@ modalLoginBtn.click(function(){
 });
 
 //modal create event
-var modalCreateEventBtn = $("#modalCreateEventBtn");
+var modalSubmitBtn = $('#modalSubmitBtn');
+var modalCreateEvent = $('#modalCreateEvent');
 
-modalCreateEventBtn.click(function(){
-  var user = firebase.auth().currentUser;
-  console.log('create Event');
-  writeNewPost(user.uid, 'test','test', 'test');
-  function writeNewPost(uid, username, title, body) {
-  // A post entry.
-  var formData = $('#createEvent').serializeObject();
-  // Get a key for a new Post.
-  var newPostKey = firebase.database().ref().child('posts').push().key;
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  var updates = {};
-  updates['/events/' + newPostKey] = formData;
-  updates['/users/' + uid + '/events/' + newPostKey] = formData;
+modalSubmitBtn.click(function(){
 
-  return firebase.database().ref().update(updates);
-}
+  var modalEventTitle = $('modalEventTitle');
+  var modalEventType = $('modalEventType');
+  var modalEventHost = $('modalEventHost');
+  var modalEventDate = $('modalEventDate');
+  var modalEventLocation = $('modalEventLocation');
+  var modalEventGuest = $('modalEventGuest');
+  var modalEventMessage = $('modalEventMessage');
+
+  //get input value
+  var modalEventTitleVal = modalEventTitle.val();
+  var modalEventTypeVal = $('#modalEventType option:selected').text();
+  var modalEventHostVal = modalEventHost.val();
+  var modalEventDateVal = modalEventDate.val();
+  var modalEventLocationVal = modalEventLocation.val();
+  var modalEventGuestVal = modalEventGuest.val();
+  var modalEventMessageVal = modalEventMessage.val();
+
+  //add issue tracker
+  var modalEventTitleTraker = new IssueTracker();
+  var modalEventTypeTraker = new IssueTracker();
+  var modalEventHostTraker = new IssueTracker();
+  var modalEventDateTraker = new IssueTracker();
+  var modalEventLocationTraker = new IssueTracker();
+  var modalEventGuestTraker = new IssueTracker();
+  var modalEventMessageTraker = new IssueTracker();
+
+  function checkrequirements(){
+
+    if(modalEventTitleVal.length<3){
+      modalEventTitleTraker.add("Event title should be greater than 3 characters.");
+    } else if(modalEventTitleVal.length>20){
+      modalEventTitleTraker.add("Event title should be fewer than 20 characters.");
+    };
+
+    if(modalEventTypeVal==="Select the type of the event"){
+      modalEventTypeTraker.add("Please select an event type.");
+    }
+
+    if(modalEventHostVal.length<3){
+      modalEventHostTraker.add("Host name should be greater than 3 characters.");
+    } else if(modalEventHostVal.length>20){
+      modalEventHostTraker.add("Host name shouold be fewer than 20 characters.");
+    }
+  }
+  console.log(modalEventTitleVal);
+  checkrequirements();
+
+  var modalEventTitleIssue = modalEventTitleTraker.retrieve();
+  var modalEventTypeIssue = modalEventTypeTraker.retrieve();
+  var modalEventHostIssue = modalEventHostTraker.retrieve();
+
+  modalEventTitle[0].setCustomValidity(modalEventTitleIssue);
+  modalEventHost[0].setCustomValidity.setCustomValidity(modalEventHostIssue);
+
+  createEvent();
+  
+  function createEvent(){
+    var user = firebase.auth().currentUser;
+    writeNewPost(user.uid);
+
+    function writeNewPost(uid) {
+    var formData = $('#createEvent').serializeObject();
+
+    console.log(formData);
+    var newPostKey = firebase.database().ref().child('posts').push().key;
+    var updates = {};
+    updates['/events/' + newPostKey] = formData;
+    updates['/users/' + uid + '/events/' + newPostKey] = formData;
+    return firebase.database().ref().update(updates);
+    }
+  }
+
+  return false;
 });
 
 
