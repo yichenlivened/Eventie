bb26fa6f60336d450c94c5328a0a3fe9b771a072
Yi Chen <easoncy435@gmail.com>
2016-08-02 13:56:43 -0400

    signup & login update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,7 @@
 $(document).ready(function () {
   'use strict';
 
-  // Initialize Firebase
-  var config = {
-    apiKey: 'AIzaSyCxRkGJI20d-SRzTlFYnmU94Jnfj7oCJbw',
-    authDomain: 'scorching-fire-2347.firebaseapp.com',
-    databaseURL: 'https://scorching-fire-2347.firebaseio.com',
-    storageBucket: 'scorching-fire-2347.appspot.com',
-  };
-
-  firebase.initializeApp(config);
-
+  //
   $.fn.serializeObject = function () {
     var o = {};
     var a = this.serializeArray();
@@ -26,201 +17,290 @@ $(document).ready(function () {
     });
     return o;
   };
+  String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  };
 
-  function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
+  var homeModel, homeController, homeView;
+  var navController, navView;
+  var requirementModel;
+  var modalSignup;
 
-  //Auth State header
-  firebase.auth().onAuthStateChanged(function (user) {
-    var headerLoginBtn = $('#headerLoginBtn');
-    var headerSignupBtn = $('#headerSignupBtn');
-    var headerUserBtn = $('#headerUserBtn');
-    var headerUsername = $('#headerUsername');
-    var headerLogoutBtn = $('#headerLogoutBtn');
-    var modalCreateEventBtn = $('#modalCreateEventBtn');
+  //HOMEPAGE
+  homeModel = {
+    config: {
+      apiKey: 'AIzaSyCxRkGJI20d-SRzTlFYnmU94Jnfj7oCJbw',
+      authDomain: 'scorching-fire-2347.firebaseapp.com',
+      databaseURL: 'https://scorching-fire-2347.firebaseio.com',
+      storageBucket: 'scorching-fire-2347.appspot.com'
+    }
+  };
 
-    if (user) {
-      firebase.database().ref('/users/' + user.uid).once('value').then(function (snapshot) {
-        headerLoginBtn.addClass('hide');
-        headerSignupBtn.addClass('hide');
-        headerUsername.text('Hi, ' + snapshot.val().username).append('<span class="caret"></span>');
-        headerUserBtn.removeClass('hide');
-      });
-      modalCreateEventBtn.off('click');
-      modalCreateEventBtn.click(function () {
-        modalCreateEvent.modal('show');
-      });
-    } else {
-      console.log('no user');
+  homeController = {
+    init: function () {
+      // Initialize Firebase
+      firebase.initializeApp(homeModel.config);
 
-      headerLoginBtn.removeClass('hide');
-      headerSignupBtn.removeClass('hide');
-      headerUserBtn.addClass('hide');
+      // Initialize homepage
+      homeView.init();
+    }
+  };
 
-      modalCreateEventBtn.off('click');
-      modalCreateEventBtn.click(function () {
-        modalSignup.modal('show');
+  homeView = {
+    init: function () {
+
+    }
+  };
+
+  homeController.init();
+
+
+  //NAV
+  navController = {
+    init: function () {
+      navView.init();
+    },
+    signOut: function () {
+      firebase.auth().signOut().then(function () {
+        console.log('logout');
+      }, function (error) {
+        console.log(error);
       });
     }
+  };
 
-    headerLogoutBtn.click(function () {
-      logout();
-      function logout() {
-        firebase.auth().signOut().then(function () {
-          console.log('logout');
-        }, function (error) {
-          console.log('error');
+  navView = {
+    init: function () {
+      this.headerLoginBtn = $('#headerLoginBtn');
+      this.headerSignupBtn = $('#headerSignupBtn');
+      this.headerUserBtn = $('#headerUserBtn');
+      this.headerUsername = $('#headerUsername');
+      this.headerLogoutBtn = $('#headerLogoutBtn');
+      this.modalCreateEventBtn = $('#modalCreateEventBtn');
+      //Auth State header
+    },
+    render: function (user) {
+      var self = this;
+      if (user) {
+        firebase.database().ref('/users/' + user.uid).once('value').then(function (snapshot) {
+          self.headerLoginBtn.addClass('hide');
+          self.headerSignupBtn.addClass('hide');
+          self.headerUsername.text('Hi, ' + snapshot.val().username).append('<span class="caret"></span>');
+          self.headerUserBtn.removeClass('hide');
+        });
+        self.headerLogoutBtn.click(function(){
+          navController.signOut();
+        });
+        self.modalCreateEventBtn.off('click');
+        self.modalCreateEventBtn.click(function () {
+          modalCreateEvent.modal('show');
+        });
+      } else {
+        console.log('no user');
+        self.headerLoginBtn.removeClass('hide');
+        self.headerSignupBtn.removeClass('hide');
+        self.headerUserBtn.addClass('hide');
+
+        self.modalCreateEventBtn.off('click');
+        self.modalCreateEventBtn.click(function () {
+          modalSignup.modal('show');
         });
       }
-    });
+    }
+  };
+
+  navController.init();
+  firebase.auth().onAuthStateChanged(function (user) {
+    navView.render(user);
   });
 
   //event table
-  firebase.database().ref('events/').once('value').then(function (snapshot) {
-    $.each(snapshot.val(), function (key, value) {
-      $('#showEvent').append('<div class="col-xs-12 col-md-6"><div class="thumbnail"><div class="caption"><p>' +
-        value.eventStart
-        + '</p><h3>' +
-        value.eventTitle
-        + '</h3><p>' +
-        value.eventLocation
-        + '</p></div></div></div>');
-    });
-  });
+  // firebase.database().ref('events/').once('value').then(function (snapshot) {
+  //   $.each(snapshot.val(), function (key, value) {
+  //     $('#showEvent').append('<div class="col-xs-12 col-md-6"><div class="thumbnail"><div class="caption"><p>' +
+  //       value.eventStart
+  //       + '</p><h3>' +
+  //       value.eventTitle
+  //       + '</h3><p>' +
+  //       value.eventLocation
+  //       + '</p></div></div></div>');
+  //   });
+  // });
 
-  function alert(type, msg) {
-    var htmlContent = '<div role="alert" class="alert alert-' + type + '">' +
-      '<span class="alert-link">' + msg + ' </span>' +
-      '</div>';
-    return htmlContent;
-  }
+  requirementModel = {
+    nameRequirement: [
+      {condition: 'value == ""', output: 'Please enter your name.'},
+      {condition: 'value.length < 3', output: 'Your name should be greater than 3 characters.'},
+      {condition: 'value.length > 20', output: 'Your name should be fewer than 20 characters.'}
+    ],
+    jobRequirement: [
+      {condition: 'value.length > 20', output: 'Your job name should be fewer than 20 characters.'}
+    ],
+    emailRequirement: [
+      {condition: 'value == ""', output: 'Please enter your email.'},
+      {condition: '!construct.validateEmail(value)', output: 'Your email format is not right.'},
+      {condition: 'value.length > 100', output: 'Your email should be fewer than 100 characters.'}
+    ],
+    passwordRequirement: [
+      {condition: 'value == ""', output: 'Please enter your password.'},
+      {condition: 'value.length < 3', output: 'Your password should be greater than 3 characters.'},
+      {condition: 'value.length > 20', output: 'Your password should be fewer than 20 characters.'}
+    ]
+  };
 
-//modal signup
-  var modalSignupBtn = $('#modalSignupBtn');
-  var modalSignup = $('#modalSignup');
-  var modalRLogin = $('#modalRLogin');
-
-  var modalSignupName = $('#modalSignupName');
-  var modalSignupJob = $('#modalSignupJob');
-  var modalSignupEmail = $('#modalSignupEmail');
-  var modalSignupPassword = $('#modalSignupPassword');
-
-  modalSignup.on('shown.bs.modal', function () {
-    modalSignupName.focus();
-  });
-
-  modalRLogin.click(function () {
-    modalSignup.modal('hide');
-    modalLogin.modal('show');
-  });
-
-
-  //get input value
-  var modalSignupNameVal = modalSignupName.val();
-  var modalSignupJobVal = modalSignupJob.val();
-  var modalSignupEmailVal = modalSignupEmail.val();
-  var modalSignupPasswordVal = modalSignupPassword.val();
-
-  modalSignupName.blur(function (e) {
-    e.preventDefault();
-
-    var modalSignupNameIssue= $('#modalSignupName-issue');
-
-    modalSignupNameVal = modalSignupName.val();
-
-    if (modalSignupNameVal == '') {
-      modalSignupNameIssue.html(alert('danger','Please enter your name.'));
-    } else if (modalSignupNameVal.length < 3) {
-      modalSignupNameIssue.html(alert('danger','Your name should be greater than 3 characters.'));
-    } else if (modalSignupNameVal && modalSignupNameVal.length > 20) {
-      modalSignupNameIssue.html(alert('danger','Your name should be fewer than 20 characters.'));
-    } else {
-      modalSignupNameIssue.html('');
-    }
-
-  });
-
-  $("#formSignup").click(function (e) {
-
-    // function createUser(name, job, email, password){
-    //   firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
-    //     writeUserData(user.uid, name, job);
-    //     console.log('successfully created the user.');
-    //     console.log(user);
-    //     modalSignup.modal('hide');
-    //   }).catch(function(error) {
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     modalSignupEmail[0].setCustomValidity(errorMessage);
-    //   });
-    // }
-    //
-    // function writeUserData(userId, name, job) {
-    //   firebase.database().ref('users/' + userId).set({
-    //     username: name,
-    //     job: job
-    //   });
-    // }
-
-  });
-
-
-//modal login
-  var modalLoginBtn = $('#modalLoginBtn');
-  var modalLogin = $('#modalLogin');
-
-  modalLoginBtn.click(function () {
-
-    var modalLoginEmail = $('#modalLoginEmail');
-    var modalLoginPassword = $('#modalLoginPassword');
-
-    //get input val value
-    var modalLoginEmailVal = $('#modalLoginEmail').val();
-    var modalLoginPasswordVal = $('#modalLoginPassword').val();
-
-    //add issue tracker
-    var modalLoginEmailTraker = new IssueTracker();
-    var modalLoginPasswordTraker = new IssueTracker();
-
-    function checkrequirements() {
-      if (!validateEmail(modalLoginEmailVal)) {
-        modalLoginEmailTraker.add('Email address is badly formatted.');
-      }
-
-      if (modalLoginPasswordVal.length < 5) {
-        modalLoginPasswordTraker.add('Password should be greater than 5 characters.');
-      } else if (modalLoginPasswordVal.length > 20) {
-        modalLoginPasswordTraker.add('Password should be fewer than 20 characters.');
+  var ConstructForm = function(modalName, inputList, type, redirect){
+    var construct = this;
+    this.controller = {
+      init: function () {
+        construct.view.init();
+      },
+      getVal: function (name) {
+        return construct.view[name].val();
+      },
+      check: function (varName) {
+        var value = this.getVal(varName);
+        var result = '';
+        var requirement = requirementModel[varName + 'Requirement'];
+        for (var i = 0; i < requirement.length; i++) {
+          if (eval(requirement[i].condition)) {
+            result = requirement[i].output;
+            break;
+          }
+        }
+        return result;
+      },
+      createUser: function(name, job, email, password){
+          var self = this;
+          firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
+            self.writeUserData(user.uid, name, job);
+            console.log('successfully created the user.');
+            console.log(user);
+            construct.view.modal.modal('hide');
+          }).catch(function(error) {
+            var errorCode = error.code;
+            alert(errorCode);
+          });
+      },
+      writeUserData: function(userId, name, job){
+          firebase.database().ref('users/' + userId).set({
+            username: name,
+            job: job
+          });
+      },
+      login: function(email, password){
+          firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+            console.log(user);
+            construct.view.modal.modal('hide');
+          }).catch(function (error) {
+            var errorMessage = error.message;
+            alert(errorMessage);
+          });
       }
     };
 
-    function login(email, password) {
-      firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
-        console.log('successfully log in.');
-        modalLogin.modal('hide');
-      }).catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        modalLoginEmail[0].setCustomValidity(errorMessage);
-      });
+    this.view = {
+      modalName: modalName,
+      inputList: inputList,
+      error: [],
+      init: function () {
+        var self = this;
+        //modal
+        this.modal = $('#' + modalName);
+
+        //form
+        inputList.forEach(function(input){
+          self[input] = $('#' + modalName + input.capitalizeFirstLetter());
+          self.addCheck(self[input], input);
+        });
+        //button
+        this.modalSubmitBtn = this.modal.find('button[type=submit]');
+        this.submit(self.modalSubmitBtn, type);
+        // set first var focus
+        this.focus(inputList[0]);
+
+        //link
+        if(redirect !== undefined){
+          this.linkLogin = this.modal.find('.link-login');
+          this.linkLogin.click(function () {
+            this.modal.modal('hide');
+            // modalSignupView.modal.modal('show');
+          });
+        }
+      },
+      alert: function (type, msg) {
+        var htmlContent = '<div role="alert" class="alert alert-' + type + '">' +
+          '<span class="alert-link">' + msg + '</span>' +
+          '</div>';
+        return htmlContent;
+      },
+      focus: function (valName) {
+        var self = this;
+        self.modal.on('shown.bs.modal', function () {
+          self[valName].focus();
+        });
+      },
+      cleanError: function () {
+        this.error = [];
+      },
+      addCheck: function (checkVar, varName) {
+        var self = this;
+        checkVar.blur(function (e) {
+          e.preventDefault();
+          self.check(checkVar, varName);
+        });
+      },
+      check: function (checkVar, varName) {
+        var self = this;
+        var result = construct.controller.check(varName);
+        if (result == '') {
+          checkVar.parent().find('.alert').remove();
+        } else {
+          self.error.push(varName);
+          checkVar.parent().find('.alert').remove();
+          checkVar.parent().append(self.alert('danger', result));
+        }
+      },
+      submit: function (btn, type) {
+        var self = this;
+        var controller = construct.controller;
+        btn.click(function (e) {
+          e.preventDefault();
+          self.cleanError();
+          self.inputList.forEach(function (e) {
+            self.check(self[e], e);
+          });
+          if (self.error.length == 0) {
+            if(type == 'signup'){
+              controller.createUser(controller.getVal('name'), controller.getVal('job'), controller.getVal('email'), controller.getVal('password'));
+            }
+            if(type == 'login'){
+              controller.login(controller.getVal('email'), controller.getVal('password'));
+            }
+          } else {
+            self[self.error[0]].focus();
+          }
+        })
+      }
+    };
+
+    this.start = function(){
+      this.controller.init();
     }
+  };
+  ConstructForm.prototype.validateEmail = function(email){
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
 
-    checkrequirements();
+  //SIGNUP MODAL
+  var modalSignUp = new ConstructForm('modalSignup', ['name', 'job', 'email', 'password'], 'signup');
+  modalSignUp.start();
 
-    var modalLoginEmailIssue = modalLoginEmailTraker.retrieve();
-    var modalLoginPasswordIssue = modalLoginPasswordTraker.retrieve();
+  //SIGNUP MODAL
+  var modalLogin = new ConstructForm('modalLogin', ['email', 'password'], 'login');
+  modalLogin.start();
 
-    if (modalLoginEmailIssue.length + modalLoginPasswordIssue.length === 0) {
-      login(modalLoginEmailVal, modalLoginPasswordVal);
-    } else {
-      modalLoginEmail[0].setCustomValidity(modalLoginEmailIssue);
-      modalLoginPassword[0].setCustomValidity(modalLoginPasswordIssue);
-    }
-    ;
 
-  });
 
 //modal create event
   var modalSubmitBtn = $('#modalSubmitBtn');
